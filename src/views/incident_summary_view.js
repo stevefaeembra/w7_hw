7347d30e9e0630afc57da85fea0bb61a0e72b2e1
Steven Kay <stevendkay@gmail.com>
2018-12-01 17:08:23 +0000

    now showing some incident details

--- src/views/incident_summary_view.js
+++ src/views/incident_summary_view.js
@@ -11,8 +11,24 @@ IncidentSummaryView.prototype.bindEvents = function () {
 IncidentSummaryView.prototype.render = function (incident) {
   // summary of the incident
   const card = document.createElement("div");
-  card.textContent= "An incident";
-  card.className = "incident-card"
+  card.className = "incident-card";
+
+  const id = incident.id;
+  const category = incident.category;
+  const lat = incident.location.latitude;
+  const lon = incident.location.longitude;
+  const name = incident.location.street.name;
+
+  const idDiv = document.createElement("div");
+  idDiv.className = "incident-card--id";
+  idDiv.textContent = id;
+  card.appendChild(idDiv);
+
+  const idCategory = document.createElement("div");
+  idCategory.className = "incident-card--category";
+  idCategory.textContent = category;
+  card.appendChild(idCategory);
+
   return card;
 };
 
